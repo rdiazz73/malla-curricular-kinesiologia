0a4a0564c745b6598f3d3ca7cf1370157ee0cacc
rdiazz73 <rosariodiazalgaze@gmail.com>
2025-07-13 22:41:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -135,14 +135,16 @@ function crearMalla() {
     const col = document.createElement("div");
     col.className = "columna-semestre";
     col.id = `col-${i}`;
+
     const titulo = document.createElement("h3");
     titulo.textContent = semestre;
     col.appendChild(titulo);
-    mallaContainer.appendChild(col);
+    mallaContainer.appendChild(col); // << IMPORTANTE: primero agregamos la columna al DOM
 
     ramos[semestre].forEach(nombre => crearTarjeta(nombre, i));
   });
 
+  // Segunda pasada: bloqueo por requisitos
   Object.keys(estadoRamos).forEach(nombre => {
     if (!requisitosAprobados(nombre)) {
       estadoRamos[nombre].bloqueado = true;
